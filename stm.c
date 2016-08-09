a5b83cb5d2b300cb0134ab516e020af1f2212d08
Jack Romo <sharrackor@gmail.com>
2016-08-09 21:02:41 +0200

    Add docs for remaining functions, add new ones

--- stm.c
+++ stm.c
@@ -132,21 +132,50 @@ void write_op_write(write_op_t write_op) {
 // readset functions
 
 
+/*
+ * new_readset: Creates an empty read set.
+ */
 readset_t new_readset() {
     //
 }
 
 
+/*
+ * readset_append: Append a read operation to the readset.
+ *
+ * Will not validate this read.
+ */
 void readset_append(readset_t readset, read_op_t read_op) {
     //
 }
 
 
+/*
+ * readset_validate_last_read: Checks whether most recent read is valid.
+ * Returns True if so, False otherwise.
+ *
+ * Is to be used right after appending a read operation.
+ */
+bool readset_validate_last_read(readset_t readset) {
+    //
+}
+
+
+/*
+ * readset_validate_all: Check whether all reads are valid.
+ * Returns True if all are valid, False otherwise.
+ *
+ * Is to be used at commit of transaction.
+ */
 bool readset_validate_all(readset_t readset) {
     //
 }
 
 
+/*
+ * readset_free_ops: Frees memory of all read operations in set.
+ * The read set can then be freed in the standard free(set) fashion.
+ */
 void readset_free_ops(readset_t readset) {
     //
 }
@@ -155,36 +184,79 @@ void readset_free_ops(readset_t readset) {
 // writeset functions
 
 
+/*
+ * new_writeset: Create a new empty write set.
+ */
 writeset_t new_writeset() {
     //
 }
 
 
+/*
+ * writeset_append: Add a new write operation to the write set.
+ *
+ * Does not validate this operation.
+ */
 void writeset_append(writeset_t writeset, write_op_t write_op) {
     //
 }
 
 
-void writeset_lock(writeset_t *writeset) {
+/*
+ * writeset_validate_last_write: Checks whether most recent write is valid.
+ * Returns True if so, False otherwise.
+ *
+ * Is to be used right after appending a write operation.
+ */
+bool writeset_validate_last_write(writeset_t writeset) {
     //
 }
 
 
+/*
+ * writeset_lock: Locks all atoms to be written to by set's write operations.
+ * If any locks fail, a nonzero value is returned rather than retrying.
+ *
+ * To be used at commit of transaction.
+ */
+int writeset_lock(writeset_t *writeset) {
+    //
+}
+
+
+/*
+ * writeset_unlock: Unlocks all atoms to be written to by set's write operations.
+ */
 void writeset_unlock(writeset_t *writeset) {
     //
 }
 
 
+/*
+ * writeset_validate_all: Validates every write set operation.
+ *
+ * Assumes writeset is already locked. To be used at commit.
+ */
 bool writeset_validate_all(writeset_t writeset) {
     //
 }
 
 
+/*
+ * writeset_commit: Commits the write operations to all written atoms.
+ *
+ * Assumes write set has already been locked.
+ */
 void writeset_commit(writeset_t writeset) {
     //
 }
 
 
+/*
+ * writeset_free_ops: Frees all write operations in the write set.
+ *
+ * After this, the write set itself can be freed with free(set) if need be.
+ */
 void writeset_free_ops(writeset_t writeset) {
     //
 }
@@ -193,61 +265,93 @@ void writeset_free_ops(writeset_t writeset) {
 // transaction functions
 
 
+/*
+ * transaction_new: Create a new empty transaction.
+ */
 transaction_t transaction_new(char *name) {
     //
 }
 
 
+/*
+ * transaction_add_read: Adds a new read operation to transaction.
+ *
+ * Does not validate this read.
+ */
 void transaction_add_read(transaction_t transaction, read_op_t read_op) {
     //
 }
 
 
+/*
+ * transaction_validate_last_read: Validates the most recent read operation in the transaction.
+ *
+ * Returns nonzero values if invalid.
+ */
 int transaction_validate_last_read(transaction_t transaction) {
     //
 }
 
 
+/*
+ * transaction_add_write: Adds a new write operation to transaction.
+ *
+ * Does not validate this write.
+ */
 void transaction_add_write(transaction_t transaction, write_op_t write_op) {
     //
 }
 
 
+/*
+ * transaction_validate_last_write: Validates the most recent write operation in the transaction.
+ *
+ * Returns nonzero values if invalid.
+ */
 int transaction_validate_last_write(transaction_t transaction) {
     //
 }
 
 
-void transaction_add_malloc(transaction_t transaction, void *pnt) {
+/*
+ * transaction_add_malloc: Add a record of the transaction using stm_malloc, and
+ * returns the address of the malloc'd memory space.
+ * This allows the transaction to free the allocated memory on abortion.
+ */
+void *transaction_add_malloc(transaction_t transaction, void *pnt) {
     //
 }
 
 
+/*
+ * transaction_add_free: Free pointer and remove record of memory being allocated within
+ * transaction.
+ *
+ * Does not work with memory allocated outside of transaction.
+ */
+void transaction_add_free(transaction_t transaction, void *pnt) {
+    //
+}
+
+
+/*
+ * transaction_abort: Abort transaction, doing all cleanup needed.
+ * This includes freeing read and write operations, along with stm_malloc'd memory.
+ *
+ * Is not responsible for returning to start of transaction.
+ */
 void transaction_abort(transaction_t transaction) {
     //
 }
 
 
+/*
+ * transaction_commit: Commit all the writes of the transaction.
+ *
+ * Will do all locking, validating, commiting and unlocking of write set along with read set.
+ * Returns a nonzero value if transaction commit fails.
+ */
 int transaction_commit(transaction_t transaction) {
     //
 }
-
-
-// utility functions
-
-
-void *stm_malloc(size_t size) {
-    //
-}
-
-
-void *stm_realloc(void *pos, size_t size) {
-    //
-}
-
-
-void stm_free(void *pos) {
-    //
-}
-
 
